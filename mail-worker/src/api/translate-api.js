20e333e5683dd142eab8b5b885e1b2d82b31968c
XS16X1 <x1@xs16.dpdns.org>
2025-09-15 00:31:57 +0800

    Update translate-api.js

--- mail-worker/src/api/translate-api.js
+++ mail-worker/src/api/translate-api.js
@@ -1,4 +1,4 @@
-import hon from "@/hono/hono.js";
+import hon from "../hono/hono.js";
 import {translate} from "../service/translate-service.js";
 
 const app = hon.post('/translate', async (c) => {
